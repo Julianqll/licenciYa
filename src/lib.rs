c6ed2e9bab2ce3633e87436eee0c573d4680ef0a
Julian Marcelo Quispe Lau Len <julianmql@hotmail.com>
2024-03-22 19:58:31 -0500

    fix: public url added for not found redirection

--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,7 @@ pub fn App() -> impl IntoView {
                 <Routes>
                     <Route path=format!("{}", env::APP_PUBLIC_URL) view=Home />
                     <Route path=format!("{}simulacro/:type", env::APP_PUBLIC_URL) view=|| view! {<Simulacro/>} />
-                    <Route path="/*any" view=NotFound />
+                    <Route path=format!("{}*any", env::APP_PUBLIC_URL) view=NotFound />
                 </Routes>
                 </main>
 
