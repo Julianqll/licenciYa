88beb4a45bb9bcf98b0515279c839ef14a4e770c
Julian Quispe <julianmql@hotmail.com>
2024-03-23 12:42:34 -0500

    fix: new try

--- src/lib.rs
+++ src/lib.rs
@@ -56,9 +56,9 @@ pub fn App() -> impl IntoView {
         >
                 <main>
                 <Routes>
-                    <Route path=format!("{}", env::APP_PUBLIC_URL) view=Home />
-                    <Route path=format!("{}:type", env::APP_PUBLIC_URL) view=|| view! {<Simulacro/>} />
-                    <Route path=format!("{}*any", env::APP_PUBLIC_URL) view=NotFound />
+                    <Route path=format!("{}/", env::APP_PUBLIC_URL) view=Home />
+                    <Route path=format!("{}/:type", env::APP_PUBLIC_URL) view=|| view! {<Simulacro/>} />
+                    <Route path=format!("{}/*any", env::APP_PUBLIC_URL) view=NotFound />
                 </Routes>
                 </main>
 
